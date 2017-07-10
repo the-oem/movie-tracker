90f2b6bf85ff9f11c46f1e9b9642a77eb9ad1164
Jason Collins <the.oem@gmail.com>
2017-07-10 14:57:18 -0600

    Initial commit

---
+++ app/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Watcher</h1>
+        <h1>Movie Watcher</h1>
+      </div>
+    )
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import MovieIndex from './components/movieIndex';
+
+ReactDOM.render(<App/>, document.getElementById('main'))
