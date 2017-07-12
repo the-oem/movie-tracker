35bddc44034c8442c4eee5cd89528b34768cbd7e
George Goering <ggoeringgg@gmail.com>
2017-07-11 18:02:48 -0600

    add URL to consts and initialize fetch helper

---
+++ app/helpers/stubs.js
@@ -0,0 +1,415 @@
+export default const newMoviesStub = {
+"results": [
+{
+"vote_count": 339,
+"id": 324852,
+"video": false,
+"vote_average": 6,
+"title": "Despicable Me 3",
+"popularity": 151.625799,
+"poster_path": "/5qcUGqWoWhEsoQwNUrtf3y3fcWn.jpg",
+"original_language": "en",
+"original_title": "Despicable Me 3",
+"genre_ids": [
+12,
+16,
+35,
+10751
+],
+"backdrop_path": "/qslbZBJHUis8PL97Dy2Fsbq8QsA.jpg",
+"adult": false,
+"overview": "Gru and his wife Lucy must stop former '80s child star Balthazar Bratt from achieving world domination.",
+"release_date": "2017-06-29"
+},
+{
+"vote_count": 2686,
+"id": 297762,
+"video": false,
+"vote_average": 7.1,
+"title": "Wonder Woman",
+"popularity": 91.455502,
+"poster_path": "/imekS7f1OuHyUP2LAiTEM0zBzUz.jpg",
+"original_language": "en",
+"original_title": "Wonder Woman",
+"genre_ids": [
+28,
+12,
+14
+],
+"backdrop_path": "/hA5oCgvgCxj5MEWcLpjXXTwEANF.jpg",
+"adult": false,
+"overview": "An Amazon princess comes to the world of Man to become the greatest of the female superheroes.",
+"release_date": "2017-05-30"
+},
+{
+"vote_count": 564,
+"id": 315635,
+"video": false,
+"vote_average": 7.4,
+"title": "Spider-Man: Homecoming",
+"popularity": 88.214875,
+"poster_path": "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg",
+"original_language": "en",
+"original_title": "Spider-Man: Homecoming",
+"genre_ids": [
+28,
+12,
+878
+],
+"backdrop_path": "/tPpYGm2mVecue7Bk3gNVoSPA5qn.jpg",
+"adult": false,
+"overview": "Following the events of Captain America: Civil War, Peter Parker, with the help of his mentor Tony Stark, tries to balance his life as an ordinary high school student in Queens, New York City, with fighting crime as his superhero alter ego Spider-Man as a new threat, the Vulture, emerges.",
+"release_date": "2017-07-05"
+},
+{
+"vote_count": 607,
+"id": 335988,
+"video": false,
+"vote_average": 6.3,
+"title": "Transformers: The Last Knight",
+"popularity": 63.851029,
+"poster_path": "/f8Ng1Sgb3VLiSwAvrfKeQPzvlfr.jpg",
+"original_language": "en",
+"original_title": "Transformers: The Last Knight",
+"genre_ids": [
+28,
+878,
+53,
+12
+],
+"backdrop_path": "/Ytv7P13rbwQ3mLpCAY8lBTqI5s.jpg",
+"adult": false,
+"overview": "Autobots and Decepticons are at war, with humans on the sidelines. Optimus Prime is gone. The key to saving our future lies buried in the secrets of the past, in the hidden history of Transformers on Earth.",
+"release_date": "2017-06-21"
+},
+{
+"vote_count": 996,
+"id": 282035,
+"video": false,
+"vote_average": 5.2,
+"title": "The Mummy",
+"popularity": 47.528982,
+"poster_path": "/zxkY8byBnCsXodEYpK8tmwEGXBI.jpg",
+"original_language": "en",
+"original_title": "The Mummy",
+"genre_ids": [
+28,
+12,
+14,
+27,
+53
+],
+"backdrop_path": "/qedJJ2z9oBYKxxO4Pp8qAkfgPst.jpg",
+"adult": false,
+"overview": "Though safely entombed in a crypt deep beneath the unforgiving desert, an ancient queen whose destiny was unjustly taken from her is awakened in our current day, bringing with her malevolence grown over millennia, and terrors that defy human comprehension.",
+"release_date": "2017-06-06"
+},
+{
+"vote_count": 1458,
+"id": 126889,
+"video": false,
+"vote_average": 5.8,
+"title": "Alien: Covenant",
+"popularity": 35.200157,
+"poster_path": "/zecMELPbU5YMQpC81Z8ImaaXuf9.jpg",
+"original_language": "en",
+"original_title": "Alien: Covenant",
+"genre_ids": [
+27,
+878,
+53
+],
+"backdrop_path": "/kMU8trT43p5LFoJ4plIySMOsZ1T.jpg",
+"adult": false,
+"overview": "Bound for a remote planet on the far side of the galaxy, the crew of the colony ship Covenant discovers what they think is an uncharted paradise, but is actually a dark, dangerous world — whose sole inhabitant is the “synthetic” David, survivor of the doomed Prometheus expedition.",
+"release_date": "2017-05-09"
+},
+{
+"vote_count": 37,
+"id": 281338,
+"video": false,
+"vote_average": 6,
+"title": "War for the Planet of the Apes",
+"popularity": 31.384836,
+"poster_path": "/y52mjaCLoJJzxfcDDlksKDngiDx.jpg",
+"original_language": "en",
+"original_title": "War for the Planet of the Apes",
+"genre_ids": [
+28,
+12,
+18,
+878
+],
+"backdrop_path": "/w4ZYFLJbiqZyvCcGHgvaHD6lTqQ.jpg",
+"adult": false,
+"overview": "Caesar and his apes are forced into a deadly conflict with an army of humans led by a ruthless Colonel. After the apes suffer unimaginable losses, Caesar wrestles with his darker instincts and begins his own mythic quest to avenge his kind. As the journey finally brings them face to face, Caesar and the Colonel are pitted against each other in an epic battle that will determine the fate of both their species and the future of the planet.",
+"release_date": "2017-07-13"
+},
+{
+"vote_count": 302,
+"id": 339403,
+"video": false,
+"vote_average": 6.6,
+"title": "Baby Driver",
+"popularity": 18.150344,
+"poster_path": "/mBkOBaxjr7IBHGuQS3sSFeyYTM.jpg",
+"original_language": "en",
+"original_title": "Baby Driver",
+"genre_ids": [
+28,
+80
+],
+"backdrop_path": "/RMjc2RAxfsi4lOq7zb8NKB10NT.jpg",
+"adult": false,
+"overview": "After being coerced into working for a crime boss, a young getaway driver finds himself taking part in a heist doomed to fail.",
+"release_date": "2017-06-28"
+},
+{
+"vote_count": 2125,
+"id": 324552,
+"video": false,
+"vote_average": 6.6,
+"title": "John Wick: Chapter 2",
+"popularity": 16.119788,
+"poster_path": "/zkXnKIwX5pYorKJp2fjFSfNyKT0.jpg",
+"original_language": "en",
+"original_title": "John Wick: Chapter 2",
+"genre_ids": [
+53,
+28,
+80
+],
+"backdrop_path": "/dQ6s3Ud2KoOs3LKw6xgZr1cw7Yq.jpg",
+"adult": false,
+"overview": "John Wick is forced out of retirement by a former associate looking to seize control of a shadowy international assassins’ guild. Bound by a blood oath to aid him, Wick travels to Rome and does battle against some of the world’s most dangerous killers.",
+"release_date": "2017-02-08"
+},
+{
+"vote_count": 311,
+"id": 339988,
+"video": false,
+"vote_average": 5.2,
+"title": "The Circle",
+"popularity": 15.611558,
+"poster_path": "/bQVqd5rWrx5GbXhJNuvKy4Viz6j.jpg",
+"original_language": "en",
+"original_title": "The Circle",
+"genre_ids": [
+18,
+53,
+878
+],
+"backdrop_path": "/hENBbG0J4zf4fFB3OVAC50poojR.jpg",
+"adult": false,
+"overview": "A young tech worker takes a job at a powerful Internet corporation, quickly rises up the company's ranks, and soon finds herself in a perilous situation concerning privacy, surveillance and freedom. She comes to learn that her decisions and actions will determine the future of humanity.",
+"release_date": "2017-04-27"
+},
+{
+"vote_count": 70,
+"id": 400136,
+"video": false,
+"vote_average": 6.7,
+"title": "Resident Evil: Vendetta",
+"popularity": 13.432408,
+"poster_path": "/88qfHWhlOBYXp69277QFhdPYnWf.jpg",
+"original_language": "ja",
+"original_title": "バイオハザード：ヴェンデッタ",
+"genre_ids": [
+28,
+12,
+16,
+27,
+9648,
+878
+],
+"backdrop_path": "/lAC9VhlVph6I0Xpm9OuSVwl8KqI.jpg",
+"adult": false,
+"overview": "BSAA Chris Redfield enlists the help of government agent Leon S. Kennedy and Professor Rebecca Chambers from Alexander Institute of Biotechnology to stop a death merchant with a vengeance from spreading a deadly virus in New York.",
+"release_date": "2017-05-27"
+},
+{
+"vote_count": 629,
+"id": 339846,
+"video": false,
+"vote_average": 5.7,
+"title": "Baywatch",
+"popularity": 10.672474,
+"poster_path": "/6HE4xd8zloDqmjMZuhUCCw2UcY1.jpg",
+"original_language": "en",
+"original_title": "Baywatch",
+"genre_ids": [
+28,
+35
+],
+"backdrop_path": "/tryI7qZHaLMVzMNpLyKHKDiZOLc.jpg",
+"adult": false,
+"overview": "Devoted lifeguard Mitch Buchannon butts heads with a brash new recruit. Together, they uncover a local criminal plot that threatens the future of the Bay.",
+"release_date": "2017-05-12"
+},
+{
+"vote_count": 282,
+"id": 260514,
+"video": false,
+"vote_average": 6.3,
+"title": "Cars 3",
+"popularity": 10.264601,
+"poster_path": "/fyy1nDC8wm553FCiBDojkJmKLCs.jpg",
+"original_language": "en",
+"original_title": "Cars 3",
+"genre_ids": [
+10751,
+35,
+16,
+12
+],
+"backdrop_path": "/rND0GSv8X2Zzs7VhyFAs9BCEmYE.jpg",
+"adult": false,
+"overview": "Blindsided by a new generation of blazing-fast racers, the legendary Lightning McQueen is suddenly pushed out of the sport he loves. To get back in the game, he will need the help of an eager young race technician with her own plan to win, inspiration from the late Fabulous Hudson Hornet, and a few unexpected turns. Proving that #95 isn't through yet will test the heart of a champion on Piston Cup Racing’s biggest stage!",
+"release_date": "2017-06-15"
+},
+{
+"vote_count": 112,
+"id": 334521,
+"video": false,
+"vote_average": 6.4,
+"title": "Free Fire",
+"popularity": 9.460946,
+"poster_path": "/dbKsjUlJCIE7wYueq0K6y4EdW0A.jpg",
+"original_language": "en",
+"original_title": "Free Fire",
+"genre_ids": [
+28,
+35,
+80,
+53
+],
+"backdrop_path": "/le2X3o9uopj1jMcMKFDKQ7MdBxb.jpg",
+"adult": false,
+"overview": "A crime drama set in 1970s Boston, about a gun sale which goes wrong.",
+"release_date": "2017-03-31"
+},
+{
+"vote_count": 66,
+"id": 269795,
+"video": false,
+"vote_average": 5.2,
+"title": "2:22",
+"popularity": 8.734929,
+"poster_path": "/sDlafquO1IvypRLbNdcC4b3oSoa.jpg",
+"original_language": "en",
+"original_title": "2:22",
+"genre_ids": [
+18,
+53
+],
+"backdrop_path": "/3zVBKPprJ9PeFBQbpT9uDiHGm61.jpg",
+"adult": false,
+"overview": "A man's life is derailed when an ominous pattern of events repeats itself in exactly the same manner every day, ending at precisely 2:22 p.m.",
+"release_date": "2017-06-29"
+},
+{
+"vote_count": 59,
+"id": 345915,
+"video": false,
+"vote_average": 5.3,
+"title": "Once Upon a Time in Venice",
+"popularity": 8.464378,
+"poster_path": "/5rr7tQfzS0ARqEea3W9SNSFGRzb.jpg",
+"original_language": "en",
+"original_title": "Once Upon a Time in Venice",
+"genre_ids": [
+28,
+35,
+53
+],
+"backdrop_path": "/qCiJk1n6qEuwYqBLfbeR74MZCZ6.jpg",
+"adult": false,
+"overview": "A Los Angeles detective seeks out the ruthless gang that stole his dog.",
+"release_date": "2017-06-08"
+},
+{
+"vote_count": 297,
+"id": 403119,
+"video": false,
+"vote_average": 4.7,
+"title": "47 Meters Down",
+"popularity": 8.439891,
+"poster_path": "/HmALxBQK8SKpOEOmqyFxOavIPR.jpg",
+"original_language": "en",
+"original_title": "47 Meters Down",
+"genre_ids": [
+53,
+27
+],
+"backdrop_path": "/6j8B3BqTuNrpAJoa0JIc7nZzOUn.jpg",
+"adult": false,
+"overview": "Two sisters on Mexican vacation are trapped in a shark observation cage at the bottom of the ocean, with oxygen running low and great whites circling nearby, they have less than an hour of air left to figure out how to get to the surface.",
+"release_date": "2017-06-15"
+},
+{
+"vote_count": 1664,
+"id": 328387,
+"video": false,
+"vote_average": 7,
+"title": "Nerve",
+"popularity": 8.0676,
+"poster_path": "/uM1dNEmcAeyDGjMcFLlkKOyBgft.jpg",
+"original_language": "en",
+"original_title": "Nerve",
+"genre_ids": [
+9648,
+12,
+80
+],
+"backdrop_path": "/a0wohltYr7Tzkgg2X6QKBe3txj1.jpg",
+"adult": false,
+"overview": "Industrious high school senior, Vee Delmonico, has had it with living life on the sidelines. When pressured by friends to join the popular online game Nerve, Vee decides to sign up for just one dare in what seems like harmless fun. But as she finds herself caught up in the thrill of the adrenaline-fueled competition partnered with a mysterious stranger, the game begins to take a sinister turn with increasingly dangerous acts, leading her into a high stakes finale that will determine her entire future.",
+"release_date": "2016-07-27"
+},
+{
+"vote_count": 48,
+"id": 289222,
+"video": false,
+"vote_average": 7,
+"title": "The Zookeeper's Wife",
+"popularity": 7.811516,
+"poster_path": "/5TVBtMhwOldZW82g1Ka11IE86wC.jpg",
+"original_language": "en",
+"original_title": "The Zookeeper's Wife",
+"genre_ids": [
+18,
+36
+],
+"backdrop_path": "/rE6W3KHdIuTdgRDLOOOMR3gfGgc.jpg",
+"adult": false,
+"overview": "The account of keepers of the Warsaw Zoo, Jan and Antonina Zabinski, who helped save hundreds of people and animals during the Nazi invasion.",
+"release_date": "2017-03-30"
+},
+{
+"vote_count": 32,
+"id": 354859,
+"video": false,
+"vote_average": 6.9,
+"title": "The Promise",
+"popularity": 6.420638,
+"poster_path": "/iDWer2VFikupdqDc7d5sxCWZ3gW.jpg",
+"original_language": "en",
+"original_title": "The Promise",
+"genre_ids": [
+10749,
+18
+],
+"backdrop_path": "/lIGxSu2QrXhOY6sQiiv5amV14wv.jpg",
+"adult": false,
+"overview": "Set during the last days of the Ottoman Empire, a love triangle develops between Mikael, a brilliant medical student, the beautiful and sophisticated artist Ana, and Chris, a renowned American journalist based in Paris.",
+"release_date": "2017-04-21"
+}
+],
+"page": 1,
+"total_results": 687,
+"dates": {
+"maximum": "2017-07-14",
+"minimum": "2017-06-02"
+},
+"total_pages": 35
+}
