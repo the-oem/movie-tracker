eb9b9530b0653cdd54fb09bb4bfc049bc0eade02
Jason Collins <the.oem@gmail.com>
2017-07-12 19:53:01 -0600

    Add router and get some routes in place.

--- app/index.js
+++ app/index.js
@@ -5,10 +5,9 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import App from './components/app.jsx';
+import AppContainer from './containers/App/AppContainer';
 import MovieIndex from './components/movieIndex.jsx';
 import rootReducer from './reducers';
-
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const history = createHistory();
@@ -20,6 +19,6 @@ const store = createStore(rootReducer,
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history} >
-      <App/>
+      <AppContainer />
     </ConnectedRouter>
   </Provider>, document.getElementById('main'));
