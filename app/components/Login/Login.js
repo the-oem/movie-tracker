8717994c5fd8b213125635af8002635e731e31ce
George Goering <ggoeringgg@gmail.com>
2017-07-14 23:32:17 -0600

    complete setup of testing environment. login mostly tested

--- app/components/Login/Login.js
+++ app/components/Login/Login.js
@@ -23,12 +23,12 @@ export default class Login extends Component {
   }
 
   render() {
-    const errorMessage = (!this.props.userIsAuthenticated && this.props.userAuthenticationFailure.error_msg !== undefined) ? 'Authentication Failure' : '';
+    const errorMessage = (!this.props.userIsAuthenticated && this.props.userAuthenticationFailure === 'User not found.') ? 'Authentication Failure' : '';
     return (
       <div className='login'>
         <form>
-          <input type='text' value={this.state.email} placeholder='Email' onChange={(e) => { this.setState({ email: e.target.value }); }}/>
-          <input type='text' value={this.state.password} placeholder='Password' onChange={(e) => { this.setState({ password: e.target.value }); }}/>
+          <input type='text' value={this.state.email} placeholder='Email' className="email-input" onChange={(e) => { this.setState({ email: e.target.value }); }}/>
+          <input type='text' value={this.state.password} placeholder='Password' className="password-input" onChange={(e) => { this.setState({ password: e.target.value }); }}/>
           <button onClick={this.submitAuthentication.bind(this)}>Login</button>
         </form>
         <h1>{errorMessage}</h1>
