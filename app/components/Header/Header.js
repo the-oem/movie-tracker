69f705be1f1a6a4a49650efd934c69612d1a9ea2
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-16 11:27:00 -0600

    Add button links to other pages

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { object } from 'prop-types';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = (props) => {
   function handleLogout() {
@@ -9,7 +10,12 @@ const Header = (props) => {
   return (
     <div className='header'>
       <h1>Movie Tracker</h1>
-      {props.userIsAuthenticated && <button onClick={handleLogout.bind(this)}>Logout</button>}
+      {props.userIsAuthenticated ?
+        <button className='header-btn' onClick={handleLogout.bind(this)}>Logout</button> :
+        <NavLink exact activeClassName="selected" to='/login'>
+          <button className='header-btn'>Login</button>
+        </NavLink>
+      }
     </div>
   );
 };
